a2d904a0c0bef687227a0e5029943d34ae6403db
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2020-09-22 19:36:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const request = require("request");
 
 app.get("/asset/:id", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "REPL.CO"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   request.get({
     url: "https://assetdelivery.roblox.com/v1/asset/?id=" + req.params.id, 
