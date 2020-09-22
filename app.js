2099c5a6fbdda91756602e2d51266c091ee156ae
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2020-09-22 17:42:32 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const app = require("express")();
+
+app.get("/asset/:id", (req, res) => {
+  request.get("https://assetdelivery.roblox.com/v1/asset/?id=" + req.params.id).on("response", resp => res.send(resp.headers["location"]));
+});
+
+app.listen(80);
