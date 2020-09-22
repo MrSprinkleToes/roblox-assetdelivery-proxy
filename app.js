8527c25d85ec1d6a89cb166326fe6c917f3066c6
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2020-09-22 19:29:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/asset/:id", (req, res) => {
     url: "https://assetdelivery.roblox.com/v1/asset/?id=" + req.params.id, 
     followAllRedirects: true
   }).on("response", resp => function(resp) {
-    res.set("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.set("Access-Control-Allow-Origin", "REPL.CO"); // update to match the domain you will make the request from
     res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(resp.request.uri.href);
   });
