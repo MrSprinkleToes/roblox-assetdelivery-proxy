797c4ca41dcb721b124b4aec9b0e4d636abdaef5
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2020-09-22 19:05:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.get("/asset/:id", (req, res) => {
   request.get({
     url: "https://assetdelivery.roblox.com/v1/asset/?id=" + req.params.id, 
     followAllRedirects: true
-  }).on("response", resp => res.send(resp.request.uri.href));
+  }).on("response", resp => function() {
+     res.set("Access-Control-Allow-Origin", "*");
+     res.send(resp.request.uri.href);
+  });
 });
 
 app.listen(process.env.PORT);
