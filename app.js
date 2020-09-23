1eb69e4b212c495ccd2ba8ca8da95bbfac1b38dd
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2020-09-22 21:18:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require("request");
 app.get("/asset/:id", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Accept-Encoding", "*");
+  res.header("Accept-Encoding", "deflate, gzip");
   request.get({
     url: "https://assetdelivery.roblox.com/v1/asset/?id=" + req.params.id, 
     followAllRedirects: true
